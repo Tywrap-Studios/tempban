fb8020447ffdd5cc5df99af657b138b57fd5092f
griinzy <kalin_stoqnov@abv.bg>
2025-03-23 02:33:27 +0200

    Updated ApiClient

--- src/main/kotlin/org/tywrapstudios/tempban/api/bandb/ApiClient.kt
+++ src/main/kotlin/org/tywrapstudios/tempban/api/bandb/ApiClient.kt
@@ -1,6 +1,7 @@
 package org.tywrapstudios.tempban.api.bandb
 
-import org.json.simple.JSONObject
+import org.json.JSONArray
+import org.json.JSONObject
 import org.tywrapstudios.tempban.Tempban
 import java.net.URI
 import java.net.http.HttpClient
@@ -11,11 +12,19 @@ import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 object ApiClient {
+
     private val httpClient = HttpClient.newHttpClient()
 
-    fun getAllBans(): JSONObject {
+    const val serverAddress = "" // get from config
 
-        TODO()
+    // TODO: HANDLE EXCEPTIONS WITH TRY-CATCH
+
+    fun getAllBans(): JSONArray {
+        val request = HttpRequest.newBuilder(URI("$serverAddress/api/ban"))
+            .GET().build()
+        val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+        val jsonRes = JSONArray(response.body())
+        return jsonRes
     }
 
     fun addBan(playerName: String, uuid: String, reason: String, bannedBy: String, bannedAt: Instant, expiresAt: Instant?): Boolean {
@@ -28,20 +37,29 @@ object ApiClient {
             put("expiresAt", expiresAt?.atOffset(ZoneOffset.UTC)?.format(DateTimeFormatter.ISO_INSTANT))
         }
 
-        try {
-            val request = HttpRequest.newBuilder(URI("")) // address goes in here
-                .POST(HttpRequest.BodyPublishers.ofString(json.toJSONString()))
-                .header("Content-type", "application/json")
-                .build()
-
+        val request = HttpRequest.newBuilder(URI("$serverAddress/api/ban"))
+            .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+            .header("Content-type", "application/json")
+            .build()
             val response = httpClient.send(request, HttpResponse.BodyHandlers.discarding())
-            if(response.statusCode() == 201) return true;
-            else return false;
-        } catch (e: Exception) {
-            // logger here
-            return false;
-        }
+            return response.statusCode() == 201;
+    }
 
-        return false;
+    fun getBan(uuid: String): JSONObject? {
+        val request = HttpRequest.newBuilder(URI("$serverAddress/api/ban/$uuid"))
+            .GET().build()
+        val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+        if(response.statusCode() == 200) {
+            val jsonRes = JSONObject(response.body())
+            return jsonRes
+        }
+        return null
+    }
+
+    fun deleteBan(uuid: String): Boolean {
+        val request = HttpRequest.newBuilder(URI("$serverAddress/api/ban/$uuid"))
+            .DELETE().build()
+        val response = httpClient.send(request, HttpResponse.BodyHandlers.discarding())
+        return response.statusCode() == 204
     }
 }
